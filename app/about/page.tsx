284977bb918fc003b1e203cfaac3a402eb7afb8f
xic343-ayushijha <68262830+xic343-ayushijha@users.noreply.github.com>
2025-08-14 13:15:16 +0530

    Initial files

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+// app/about/page.js file
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to the about page!</p>
+        </div>
+    );
+}
